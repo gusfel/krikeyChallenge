1518645fc3e4636e42c9a4c4c53e51dc5f3d823f
gusfel <agustinjfeliciano@gmail.com>
2021-05-05 16:15:29 -0700

    got server connected to db

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const db = require('./db.js');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.get('/author', (req, response) => {
+  db.connect((err, client, done) => {
+    if (err) { throw err; }
+    client.query('SELECT * FROM authors', (err, res) => {
+      done();
+      if (err) {
+        console.log(err.stack)
+      } else {
+        response.send(res.rows[0])
+      }
+    })
+  })
 });
 
 app.listen(port, () => {
