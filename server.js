092bb2a4fdbefd63f44795bd2a52d9f0aadd504c
gusfel <agustinjfeliciano@gmail.com>
2021-05-06 10:50:07 -0700

    added caching functionality to server

--- server.js
+++ server.js
@@ -2,23 +2,44 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const NodeCache = require( 'node-cache' );
+const myCache = new NodeCache( { stdTTL: 100, checkperiod: 120 } );
+
 const queries = require('./queries.js');
 
 app.get('/author', (request, response) => {
   const authorName = request.query.author_name;
-  const dataResponse = (err, data) => {
-    if (err) { throw err; }
-    if (data.length) {
-      response.send(data);
-    } else {
-      response.status(404).send('Not Found');
-    }
-  };
 
   if (authorName) {
-    queries.getOne(authorName, dataResponse);
+    const value = myCache.get( authorName );
+    if (value) {
+      response.send(value);
+    } else {
+      queries.getOne(authorName, (err, data) => {
+        if (err) { throw err; }
+        if (data.length) {
+          myCache.set(authorName, data);
+          response.send(data);
+        } else {
+          response.status(404).send('Not Found');
+        }
+      });
+    }
   } else {
-    queries.getTotals(dataResponse);
+    const value = myCache.get('top10');
+    if (value) {
+      response.send(value);
+    } else {
+      queries.getTotals((err, data) => {
+        if (err) { throw err; }
+        if (data.length) {
+          myCache.set('top10', data);
+          response.send(data);
+        } else {
+          response.status(404).send('Not Found');
+        }
+      });
+    }
   }
 });
 
